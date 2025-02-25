f956d50d9326086be6dc7ea3adfe15bf7aca0b7f
Subesh11 <subeshgumanju9@gmail.com>
2025-02-25 21:12:44 +0545

    first

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApps.Models;
+
+namespace MyApps.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public IActionResult Overview()
+        {
+            var Item = new Item() { Name = "KeyBoard" };
+            return View(Item);
+        }
+        public IActionResult Test(int id)
+        {
+            return Content("id=" + id);
+
+        }
+    }
+}
